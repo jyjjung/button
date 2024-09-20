4ddc7456f0a8436dbacb9da44111ab3b9c123be9
jyjjung <yejoon7154@gmail.com>
2024-09-20 16:32:45 +1000

    Added more functions

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import { useState } from "react";
+
 export default function MyApp() {
+  const [count, setCount] = useState(0);
+
+  function numberPlus() {
+    setCount(count + 1);
+  }
+
+  function numberMinus() {
+    setCount(count - 1);
+  }
+
+  function numberReset() {
+    alert("Your count has been reset.");
+    setCount(0);
+  }
+
   return (
     <div>
-      <MyButton />
+      <ButtonPlus count={count} onClick={numberPlus} />
+      <p>{count}</p>
+      <ButtonMinus count={count} onClick={numberMinus} />
+      <ButtonReset count={count} onClick={numberReset} />
     </div>
   );
 }
 
-function MyButton() {
-  const [count, setCount] = useState(0);
+function ButtonPlus({ count, onClick }) {
+  return <button onClick={onClick}>+</button>;
+}
 
-  function handleClick() {
-    setCount(count + 1);
-  }
+function ButtonMinus({ count, onClick }) {
+  return <button onClick={onClick}>-</button>;
+}
 
-  return <button onClick={handleClick}>Clicked {count} times</button>;
+function ButtonReset({ count, onClick }) {
+  return <button onClick={onClick}>Reset</button>;
 }
