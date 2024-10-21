2330785acb39806e117644ebee75d389f379b419
Ye-Joon Jung <ye-joonjung@192-168-1-106.tpgi.com.au>
2024-10-21 16:04:24 +1000

    Updated feature

--- src/App.js
+++ src/App.js
@@ -2,34 +2,67 @@ import { useState } from "react";
 import "./App.css";
 
 const MyApp = () => {
+  const [num, setNum] = useState(0);
+
+  const randomNumberInRange = (min, max) => {
+      return Math.floor(Math.random()
+          * (max - min + 1)) + min;
+  };
+
+  const handleClick = () => {
+      setNum(randomNumberInRange(1, 100));
+  };
+
   const [count, setCount] = useState(0);
 
   const numberPlus = () => {
-    setCount(count + 1);
+    setCount(Math.round((count+Math.random()) * 100) / 100);
   };
 
   const numberMinus = () => {
-    setCount(count - 1);
+    setCount(Math.round((count-Math.random()) * 100) / 100);
   };
 
-  const numberReset = () => {
+  const numberRandom = () => {
+    setCount(Math.round((count+count*Math.random()) * 100) / 100);
+  };
+
+  const numberReset = (event) => {
     setCount(0);
   };
 
-  const numberChange = (event) => {
-    setCount(prompt("Please enter your new number") - 1 + 1);
-  };
+  if(count>100){
+    setCount(0)
+    alert("Your number was above 100. Please try again.")
+  }
+
+  if(count<0){
+    setCount(0)
+    alert("Your number was below 0. Please try again.")
+  }
 
   return (
     <div>
-      <p>{count}</p>
-      <div class="plusMinus">
-        <ButtonPlus count={count} onClick={numberPlus} />
-        <ButtonMinus count={count} onClick={numberMinus} />
-      </div>
-      <ButtonReset count={count} onClick={numberReset} />
-      <ButtonChangeNumber count={count} onClick={numberChange} />
-    </div>
+      <body>
+      <h2>Get the counter to {num}.</h2>
+        <p>{count}</p>
+      </body>
+        <div class="buttons">
+          <ButtonMinus count={count} onClick={numberMinus} />
+          <ButtonPlus count={count} onClick={numberPlus} />
+          <ButtonRandom count={count} onClick={numberRandom} />
+          <ButtonReset count={count} onClick={numberReset} />
+        </div>
+        <div>
+          <progress value={(count/num)} />
+          <progress value={(100-count)/(100-num)} />
+        </div>
+        <div>
+            <button onClick={handleClick}>
+                GENERATE NEW GOAL
+            </button>
+        </div>
+</div>
   );
 };
 
@@ -41,11 +74,12 @@ const ButtonMinus = ({ count, onClick }) => {
   return <button onClick={onClick}>-</button>;
 };
 
+const ButtonRandom = ({ count, onClick }) => {
+  return <button onClick={onClick}>Random</button>;
+};
+
 const ButtonReset = ({ count, onClick }) => {
   return <button onClick={onClick}>Reset</button>;
 };
 
-const ButtonChangeNumber = ({ count, onClick }) => {
-  return <button onClick={onClick}>Change number</button>;
-};
 export default MyApp;
