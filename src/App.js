bc69ef0926f1e81b9a2511788af6b27fd678d14f
Ye-Joon Jung <ye-joonjung@192-168-1-106.tpgi.com.au>
2024-10-23 17:15:11 +1000

    Updated features

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import "./App.css";
+import { waitFor } from "@testing-library/react";
 
 const MyApp = () => {
   const [num, setNum] = useState(0);
+  const [click, setClick] = useState(0);
+  const [count, setCount] = useState(0);
 
   const randomNumberInRange = (min, max) => {
       return Math.floor(Math.random()
@@ -10,25 +13,29 @@ const MyApp = () => {
   };
 
   const handleClick = () => {
-      setNum(randomNumberInRange(1, 100));
-  };
-
-  const [count, setCount] = useState(0);
+    setNum(randomNumberInRange(1, 99));
+    setCount(0);
+    setClick(0);
+};
 
   const numberPlus = () => {
     setCount(Math.round((count+Math.random()) * 100) / 100);
+    setClick(click+1);
   };
 
   const numberMinus = () => {
     setCount(Math.round((count-Math.random()) * 100) / 100);
+    setClick(click+1);
   };
 
   const numberRandom = () => {
     setCount(Math.round(((Math.random()) * 100))/100+Math.round(Math.random()*100));
+    setClick(click+1);
   };
 
   const numberReset = (event) => {
     setCount(0);
+    setClick(click+1);
   };
 
   if(count>100){
@@ -41,35 +48,57 @@ const MyApp = () => {
     alert("Your number was below 0. Please try again.");
   }
 
+  if(click>100){
+    alert("You used more than 100 functions. Please try again.")
+    setNum(randomNumberInRange(1, 99));
+    setCount(0);
+    setClick(0);
+  }
+
   document.onkeydown = function(e) {
     switch (e.keyCode) {
         case 37:
           setCount(Math.round((count-Math.random()) * 100) / 100);
+          setClick(click+1);
             break;
         case 38:
           setCount(Math.round(((Math.random()) * 100))/100+Math.round(Math.random()*100));
+          setClick(click+1);
             break;
         case 39:
           setCount(Math.round((count+Math.random()) * 100) / 100);
+          setClick(click+1);
             break;
         case 40:
           setCount(0);
+          setClick(click+1);
             break;
        case 32:
-          setNum(randomNumberInRange(1, 100));
+          setNum(randomNumberInRange(1, 99));
+          setClick(0);
+          setCount(0);
             break;
+            
+       case 82:
+         setNum(randomNumberInRange(1, 99));
+         setClick(0);
+         setCount(0);
+           break;
     }
-};
-
-if(count<num+0.001 &&count>num-0.001){
-  alert("You win! 🎉🎉🎉");
 }
+
+if(count<num+0.001 &&count>num-0.001 &&count>0){
+  alert("You win!");
+};
 
   return (
     <div>
       <body>
       <h2>Get the counter to {num}.</h2>
         <p>{count}</p>
+        <p class="rule">NUMBER OF FUNCTIONS USED: {click}</p>
+        <progress value={(100-click)/100} />
+        <p class="rule">NUMBER OF FUNCTIONS LEFT: {100-click}</p>
       </body>
         <div class="buttons">
           <ButtonMinus count={count} onClick={numberMinus} />
@@ -77,13 +106,12 @@ if(count<num+0.001 &&count>num-0.001){
           <ButtonRandom count={count} onClick={numberRandom} />
           <ButtonReset count={count} onClick={numberReset} />
         </div>
-        <div>
-          <progress value={(count/num)} />
-          <progress value={((100-count)/(100-num))} />
-        </div>
+        <p>
+
+        </p>
         <div class="buttons">
-            <button onClick={handleClick}>
-                GENERATE NEW GOAL
+            <button class="reset" onClick={handleClick}>
+                RESET GAME
             </button>
         </div>
 </div>
@@ -103,7 +131,7 @@ const ButtonRandom = ({ count, onClick }) => {
 };
 
 const ButtonReset = ({ count, onClick }) => {
-  return <button onClick={onClick}>Reset</button>;
+  return <button onClick={onClick}>Reset count</button>;
 };
 
 export default MyApp;
